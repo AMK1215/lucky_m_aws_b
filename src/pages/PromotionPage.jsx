f6754d22c5b652a48366559ba8a1414e4814248c
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-10-31 15:39:58 +0700

    a

--- src/pages/PromotionPage.jsx
+++ src/pages/PromotionPage.jsx
@@ -7,6 +7,8 @@ import BASE_URL from "../hooks/baseURL";
 
 const PromotionPage = () => {
   const { data: promotions } = useFetch(BASE_URL + "/promotion");
+  console.log(promotions);
+  
   return (
     <div className="pt-4 pb-5 mb-5 px-2 px-sm-3 px-lg-5 text-white">
       <div className="d-flex align-items-center justify-content-center gap-2 text-center w-semibold mb-4">
@@ -17,7 +19,7 @@ const PromotionPage = () => {
         return (
           <div key={index} className="mb-4">
             <div className="text-center mb-4">
-              <img src={item.img_url} className="mx-auto img-fluid rounded-4" />
+              <img src={item.image} className="mx-auto img-fluid rounded-4" />
             </div>
             {/* <p className="mt-2 px-2 px-sm-4">{item.title}</p> */}
           </div>
